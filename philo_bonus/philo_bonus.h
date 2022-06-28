0d6be38c2f730a7db8d15b9754d8bf1da468322e
Salah-eddine Harrachmin <sharrach@e3r8p3.1337.ma>
2022-06-28 14:13:41 +0100

    bonus

--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -6,7 +6,20 @@
 /*   By: sharrach <sharrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 18:34:58 by sharrach          #+#    #+#             */
-/*   Updated: 2022/06/27 18:34:59 by sharrach         ###   ########.fr       */
+/*   Updated: 2022/06/28 14:13:28 by sharrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/time.h>
+
+
+long long	find_time(void);
+int	ft_atoi(const char *str);
+
+#endif
--- philo_bonus/utils_bonus.c
+++ philo_bonus/utils_bonus.c
@@ -6,7 +6,41 @@
 /*   By: sharrach <sharrach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 18:35:02 by sharrach          #+#    #+#             */
-/*   Updated: 2022/06/27 18:35:03 by sharrach         ###   ########.fr       */
+/*   Updated: 2022/06/28 14:13:26 by sharrach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo_bonus.h"
+
+long long	find_time(void)
+{
+	struct timeval	t;
+
+	gettimeofday(&t, NULL);
+	return ((t.tv_sec * 1000) + (t.tv_usec / 1000));
+}
+
+int	ft_atoi(const char *str)
+{
+	long	i;
+	long	s;
+	long	result;
+
+	result = 0;
+	s = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-')
+		s *= -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] && str[i] <= '9' && str[i] >= '0')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	result *= s;
+	return (result);
+}
